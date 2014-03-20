4fd0ed6e120fc21db25f9183e00d66e1019b1e0a
Fabian Schindler <fabian.schindler@gmx.at>
2014-03-20 18:21:40 +0100

    Added install script.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name='pygdal',
+    version='0.0.1dev',
+    description='Python Distribution Utilities',
+    author='Fabian Schindler',
+    author_email='fabian.schindler@eox.at',
+    url='https://github.com/constantinius/pygdal',
+    packages=['pygdal'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: GIS"
+    ]
+
+)
